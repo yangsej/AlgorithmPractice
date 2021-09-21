624cb4a41309c5c5953a0c353aaea3c466fee2bb
yangsej <yangsej@gmail.com>
2021-09-21 11:26:39 +0900

    바킹독 문제집(https://www.acmicpc.net/workbook/by/BaaaaaaaaaaarkingDog) 0x01강 - 시간복잡도와 기초 지식 연습문제_구버전 [15/26]

--- Baekjoon/P10093.java
+++ Baekjoon/P10093.java
@@ -1,18 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
+import java.util.stream.Collectors;
 
 public class P10093 {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
         StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
 
-        int N = Integer.parseInt(tokenizer.nextToken());
+        long A = Long.parseLong(tokenizer.nextToken()), B = Long.parseLong(tokenizer.nextToken());
 
         reader.close();
 
-        int answer = 0;
+        if(A > B){
+            long temp = A;
+            A = B;
+            B = temp;
+        }
+
+        int length = 0;
+        if(A != B){
+            length = (int) (B - A - 1);
+        }
+        System.out.println(length); // 같은 수면 0
+
+        long[] nums = new long[length];
+        for(int i=0; i < length; i++){
+            nums[i] = A + i + 1;
+        }
+
+        String answer = Arrays.stream(nums).mapToObj(String::valueOf).collect(Collectors.joining(" "));
 
         System.out.println(answer);
     }

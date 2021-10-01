84c075015507005d4825897caa4a9fe7dbff72c6
BuildTools <yangsej>
2021-10-02 00:00:51 +0900

    바킹독 문제집(https://www.acmicpc.net/workbook/by/BaaaaaaaaaaarkingDog) 0x05강 - BFS, DFS_구버전 [7/24]

--- Baekjoon/BASE_BUFFER.java
+++ Baekjoon/BASE_BUFFER.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
-public class BASE {
+public class BASE_BUFFER {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
 

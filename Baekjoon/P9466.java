fc8719871d1e76d3bf4b882ab5b8c9c6d6862bdc
yangsej <yangsej@gmail.com>
2021-11-07 18:53:32 +0900

    [BaaaaaaaaaaarkingDog의 공개 문제집](https://www.acmicpc.net/workbook/by/BaaaaaaaaaaarkingDog) 0x05강 - BFS, DFS_구버전 [19/41]
    + [P9466 텀 프로젝트](https://www.acmicpc.net/problem/9466)

--- Baekjoon/P9466.java
+++ Baekjoon/P9466.java
@@ -1,5 +1,4 @@
 import java.io.IOException;
-import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -12,38 +11,39 @@ public class P9466 {
         for (int t = 0; t < T; t++) {
             int N = sc.nextInt(), answer = N;
             int[] students = new int[N+1], visit = new int[N+1];
-            HashSet<Integer> set = new HashSet<>();
 
             for (int n = 1; n <= N; n++) {
                 students[n] = sc.nextInt();
 
                 if(students[n] == n){
                     visit[n] = n;
-                    set.add(n);
                     answer--;
                 }
             }
 
             for (int n = 1; n <= N; n++) {
-                if(set.contains(visit[n])) continue;
+                if(visit[n] != 0) continue;
                 visit[n] = n;
 
-                Stack<Integer> stack = new Stack<Integer>();
+                Stack<Integer> stack = new Stack<>();
                 stack.add(n);
 
                 while(!stack.isEmpty()){
                     int student = stack.peek(), next = students[student];
 
-                    if(n == next){
-                        answer -= stack.size();
-                        set.add(n);
+                    if(visit[next] == n){
+                        while(student != next){
+                            student = stack.pop();
+                            answer--;
+                            visit[student] = next;
+                        }
                         break;
+                    } else if(visit[next] != 0) {
+                        break;
+                    } else{
+                        visit[next] = n;
+                        stack.add(next);
                     }
-                    if(set.contains(visit[next])) break;
-                    if(visit[student] == visit[next]) break;
-
-                    visit[next] = n;
-                    stack.add(students[student]);
                 }
             }
 

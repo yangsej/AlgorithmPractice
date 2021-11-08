64c940caae7eac8e23be7eec409663392e81f13f
yangsej <yangsej@gmail.com>
2021-11-08 22:27:34 +0900

    [BaaaaaaaaaaarkingDog의 공개 문제집](https://www.acmicpc.net/workbook/by/BaaaaaaaaaaarkingDog) 0x05강 - BFS, DFS_구버전 [20/41]
    + [P2206 벽 부수고 이동하기](https://www.acmicpc.net/problem/2206)

--- Baekjoon/P2206.java
+++ Baekjoon/P2206.java
@@ -19,51 +19,46 @@ public class P2206 {
 
         int[][] DIRS = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
 
-        Queue<Integer> rq = new LinkedList<>(), cq = new LinkedList<>(), destq = new LinkedList<>();
-        rq.add(0);
-        cq.add(0);
-        destq.add(1);
+        Queue<int[]> Q = new LinkedList<>();
+        Q.add(new int[] {0, 0, 0});
 
-        int[][] visit = new int[N][M], visit_used = new int[N][M];
-        visit[0][0] = 1;
+        int[][][] visit = new int[2][N][M];
+        visit[0][0][0] = 1;
+        visit[1][0][0] = 1;
 
-        while(!rq.isEmpty()){
-            int r = rq.poll(), c = cq.poll(), d = destq.poll();
+        while(!Q.isEmpty()){
+            int[] coord = Q.poll();
+            int r = coord[0], c = coord[1], w = coord[2];
+
+            if((r == N-2 && c == M-1) || (r == N-1 && c == M-2)){
+                visit[w][N-1][M-1] = visit[w][r][c] + 1;
+                break;
+            }
+
 
             for (int[] dir : DIRS) {
-                int nr = r + dir[0], nc = c + dir[1], nd = d;
+                int nr = r + dir[0], nc = c + dir[1];
 
                 if(nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
 
-                if(arr[nr][nc] == '1'){
-                    if(nd == 0) continue;
-                    nd = 0;
-
-                    if(visit_used[nr][nc] > 0) continue;
-                    visit_used[nr][nc] = visit[r][c] + 1;
-                } else{
-                    if(nd == 1) {
-                        if(visit[nr][nc] > 0) continue;
-                        visit[nr][nc] = visit[r][c] + 1;
-                    }
-                    else {
-                        if(visit_used[nr][nc] > 0) continue;
-                        visit_used[nr][nc] = visit_used[r][c] + 1;
+                if(arr[nr][nc] == '0'){
+                    if(visit[w][nr][nc] == 0 || visit[w][r][c] + 1 < visit[w][nr][nc]) {
+                        visit[w][nr][nc] = visit[w][r][c] + 1;
+                        Q.add(new int[] {nr, nc, w});
                     }
                 }
 
-                if(nr == N-1 && nc == M-1){
-                    break;
+                if(arr[nr][nc] == '1' && w == 0){
+                    if (visit[1][nr][nc] == 0 || visit[1][r][c] + 1 < visit[1][nr][nc]) {
+                        visit[1][nr][nc] = visit[0][r][c] + 1;
+                        Q.add(new int[]{nr, nc, 1});
+                    }
                 }
-
-                rq.add(nr);
-                cq.add(nc);
-                destq.add(nd);
             }
         }
 
-        int answer = visit[N-1][M-1];
-        if(answer == 0) answer = visit_used[N-1][M-1];
+        int answer = visit[0][N-1][M-1];
+        if(answer == 0) answer = visit[1][N-1][M-1];
         if(answer == 0) answer = -1;
 
         System.out.println(answer);
